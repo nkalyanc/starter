f58c0fcaa0cadb0592eb1959566362ef52f8616e
Kalyana Nookala <nckalyan@in.ibm.com>
2019-10-17 19:04:34 +0530

    updated GIT hub URL - updated again now

--- multiplybyfactor.py
+++ multiplybyfactor.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 # This URL must be accessible via pip install
 
 PACKAGE_URL = 'git+https://github.com/nkalyanc/starter/functions@'
-# I updated above URL -1
+# I updated above URL -1 -- is this updated?
 
 class MultiplyByFactor(BaseTransformer):
 
