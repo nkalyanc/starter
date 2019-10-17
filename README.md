eec42a7d83ba2a42d64acc346c4923528cdc86b9
Kalyana Nookala <nckalyan@in.ibm.com>
2019-10-17 15:01:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Empty Python Package
+
+This package is empty. 
+
+To install this package: 
+
+```
+pip install git+git://github.com/madendorff/functions --upgrade
+```
+
+Prereqs are:
+
+iotfunctions. See https://github.com/ibm-watson-iot/functions
+
---
+++ multiplybyfactor.py
@@ -0,0 +1,49 @@
+import inspect
+import logging
+import datetime as dt
+import math
+from sqlalchemy.sql.sqltypes import TIMESTAMP,VARCHAR
+import numpy as np
+import pandas as pd
+
+from iotfunctions.base import BaseTransformer
+from iotfunctions import ui
+
+logger = logging.getLogger(__name__)
+
+# Specify the URL to your package here.
+# This URL must be accessible via pip install
+
+PACKAGE_URL = 'git+https://github.com/<repo>/functions@'
+
+class MultiplyByFactor(BaseTransformer):
+
+    def __init__(self, input_items, factor, output_items):
+
+        self.input_items = input_items
+        self.output_items = output_items
+        self.factor = float(factor)
+        super().__init__()
+    def execute(self, df):
+        df = df.copy()
+        for i,input_item in enumerate(self.input_items):
+            df[self.output_items[i]] = df[input_item] * self.factor
+        return df
+
+    @classmethod
+    def build_ui(cls):
+        #define arguments that behave as function inputs
+        inputs = []
+        inputs.append(ui.UIMultiItem(
+                name = 'input_items',
+                datatype=float,
+                description = "Data items adjust",
+                output_item = 'output_items',
+                is_output_datatype_derived = True)
+                      )
+        inputs.append(ui.UISingle(
+                name = 'factor',
+                datatype=float)
+                      )
+        outputs = []
+        return (inputs,outputs)
